7810f22d8d4a3144acbd0a78f59aadf65d8d9501
AbirElgaddar <a.elggaddar@gmail.com>
2024-04-16 15:42:27 +0100

    Initial commit

---
+++ src/main/java/pres/SpringXML.java
@@ -0,0 +1,13 @@
+package pres;
+
+import metier.IMetier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringXML {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        IMetier metier = (IMetier) context.getBean("IMetier");
+        System.out.println(metier.calculation());
+    }
+}
